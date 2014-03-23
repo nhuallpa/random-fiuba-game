451efa9696f5068141f8614ce058906b313f6353
erikschmoll@gmail.com <erikschmoll@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-03-23 12:55:14 +0000

    Podio: Eventos
    Descripción: La funcionalidad principal para la primera parte... abarca en un 100%
    
    Interface para la clase cliente:
            Detector detector; // Objeto que conoce los eventos
    	...
    	while(isContinue){ // Bucle del cliente (principal del juego)
    		detectorEvent.detect(); // Actualiza el estado de los eventos ocurridos
    		isContinue = !detectorEvent.isQuit(); // Indica si finaliza el juego
                    ...
    		if(detectorEvent.isBeginLife()){   
    			//DAR ANIMACION
    		}
                    ...
    		if(detectorEvent.isRegenerateWorld()){
    			//Regenerar el mundo
    		}
                    ...
    		if(detectorEvent.isPuase()){
    			//El juego esta en pausa hasta que isPause diga false
    		}
                    ...
    		detectorEvent.clearStates(); // Es necesario para el buen funcionamieto de cada iteración,
    	}
            ...

--- trunk/randomgame/src/Cliente/controlador/SButton.cpp
+++ trunk/randomgame/src/Cliente/controlador/SButton.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 SButton::SButton(void)
 {
-	m_state = BUTTON_POP;
+	buttonState = BUTTON_POP;
+	prevAction = REGENERATE_WORLD;
 }
 
 
@@ -14,10 +15,32 @@ SButton::~SButton(void)
 
 void SButton::execute(const Uint8* keys){
 	if(keys[SDL_SCANCODE_S]){
-		m_state = BUTTON_PUSH;
+		buttonState = BUTTON_PUSH;
 	}
 	else{
-		m_state = BUTTON_POP;
+		buttonState = BUTTON_POP;
 	}
 	return;
+}
+
+bool SButton::isBeginLife(){
+	return  ((prevAction == REGENERATE_WORLD)
+		  && (buttonState == BUTTON_PUSH));
+}
+
+bool SButton::isRegenerateWorld(){
+	return  ((prevAction == BEGIN_LIFE)
+		  && (buttonState == BUTTON_PUSH));
+}
+
+void SButton::cleanState(){
+	if(buttonState == BUTTON_PUSH){	
+		if(prevAction == REGENERATE_WORLD){
+			prevAction = BEGIN_LIFE;
+		}
+		else{
+			prevAction = REGENERATE_WORLD;
+		}
+	}
+	buttonState = BUTTON_POP;
 }
