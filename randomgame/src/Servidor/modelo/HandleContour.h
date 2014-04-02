5d4b3ea98f62f19eac6714de4b8c21ab17fe5619
erikschmoll@gmail.com <erikschmoll@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-04-02 20:52:09 +0000

    Podio: Identificación de vértices/Polìgonos
    Descripción: -,

--- randomgame/src/Servidor/modelo/HandleContour.h
+++ randomgame/src/Servidor/modelo/HandleContour.h
@@ -5,6 +5,7 @@
 
 #include <list>
 #include "Point.h"
+#include <Box2D.h>
 
 using namespace std;
 using namespace server_model_point;
@@ -15,8 +16,23 @@ namespace server_model_handle{
 		HandleContour();
 		~HandleContour();
 		list<Point*> rdp(list<Point*> contour, int epsilon);
+		int Validate(list<b2Vec2*> l);
 	private:
 		float findPerpendicularDistance(Point* p, Point* p1, Point* p2);
+		float det(float x1,float y1,
+			      float x2,float y2,
+				  float x3,float y3);
+		b2Vec2* hitSegment(float x1,float y1,
+						  float x2,float y2,
+						  float x3,float y3,
+						  float x4,float y4);
+		bool isOnSegment(float px,float py,
+			             float x1,float y1,
+			             float x2,float y2);
+		bool isOnLine(float px,float py,
+			          float x1,float y1,
+			          float x2,float y2);
+		list<b2Vec2*> calcShapes(list<b2Vec2*> l);
 	};
 }
 #endif /*__HANDLECONTOUR__*/
