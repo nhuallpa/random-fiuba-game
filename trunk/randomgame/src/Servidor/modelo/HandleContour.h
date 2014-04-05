cf7a4ea5d29e3c610efa7861176299ef24cd8d10
erikschmoll@gmail.com <erikschmoll@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-04-05 11:51:04 +0000

    Podio: Identificación de vértices/Polìgonos
    Descripción: Etapa de Test,

--- trunk/randomgame/src/Servidor/modelo/HandleContour.h
+++ trunk/randomgame/src/Servidor/modelo/HandleContour.h
@@ -3,9 +3,9 @@
 #ifndef __HANDLECONTOUR__
 #define __HANDLECONTOUR__
 
-#include <list>
 #include <Box2D.h>
 #include <vector>
+#include "b2Separator.h"
 
 using namespace std;
 
@@ -14,12 +14,18 @@ namespace server_model_handle{
 	public:
 		HandleContour();
 		~HandleContour();
-		vector<vector<b2Vec2>> getPolygonConvex(list<b2Vec2*> contour, int scale);
+		vector<vector<b2Vec2>> getPolygonConvex(vector<b2Vec2> contour,
+			                                    float epsilon, 
+												int scale);
 	private:
-		list<b2Vec2*> rdp(list<b2Vec2*> contour, int epsilon);
-		float findPerpendicularDistance(b2Vec2* p, b2Vec2* p1, b2Vec2* p2);
-		vector<b2Vec2> change(list<b2Vec2*> contour, int scale);
 		
+		vector<b2Vec2> rdp(vector<b2Vec2> contour, float epsilon);
+		float findPerpendicularDistance(b2Vec2 p, b2Vec2 p1, b2Vec2 p2);
+		//vector<b2Vec2> change(list<b2Vec2*> contour, int scale);
+		/*vector<vector<b2Vec2>> getPolygonConvex(vector<b2Vec2> contour,float epsilon, b2Separator* sep);
+		vector<vector<b2Vec2>> split(vector<b2Vec2> contour);
+		vector<b2Vec2> getItems(vector<b2Vec2>* contour, int cant);
+		*/
 	};
 }
 #endif /*__HANDLECONTOUR__*/
