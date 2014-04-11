4028eb6fc61f045ffafb2f12611bf2f9bacb50d0
Bautista.Tusca@gmail.com <Bautista.Tusca@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-04-11 01:14:39 +0000

    Revisión de bugs de Excepciones.

--- randomgame/src/Servidor/modelo/Exceptions/ContourException.h
+++ randomgame/src/Servidor/modelo/Exceptions/ContourException.h
@@ -13,7 +13,7 @@ namespace server_model_exp{
 	class ContourExp : public exception{
 
 		pair<int,string> e;
-
+	public:
 		ContourExp(pair<int,string> error) : exception(), e(error) {}
 		const char* what() const throw();
 		
