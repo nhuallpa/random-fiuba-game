c83e0ec78f8317a2512efeae9b9156677c929514
arielik@gmail.com <arielik@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-04-12 00:21:01 +0000

    Modificada la velocidad de los cuerpos

--- randomgame/src/Servidor/modelo/GameEngine.cpp
+++ randomgame/src/Servidor/modelo/GameEngine.cpp
@@ -155,9 +155,9 @@ float GameEngine::getTimeStep () {
 bool GameEngine::step(){
 
 	//animateJoints();
-	float32 timeStep = (1.0f/120.0f);
+	float32 timeStep = (1.0f/20.0f);
 	//Simulo (1 step) - default values Box2D
-	this->myWorld->Step(timeStep,2,2);
+	this->myWorld->Step(timeStep,8,3);
 
 	//Reflect model
 	std::list<Body*>::const_iterator iterator;
