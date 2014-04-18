c91909163f9116f1318cedcfc438bd12f7d0f785
erikschmoll@gmail.com <erikschmoll@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-04-18 20:12:04 +0000

    Estados: Patron State, con refoctor
    Descripcion: Agrego que se reinicie si el juego esta en pausa

--- trunk/randomgame/src/Cliente/controlador/StopGame.cpp
+++ trunk/randomgame/src/Cliente/controlador/StopGame.cpp
@@ -23,7 +23,9 @@ StopGame::~StopGame(void){}
 State* StopGame::execute(SDL_Event* e, const Uint8* keys){
 	State* st = StopGame::getInstance();
 	if(keys[SDL_SCANCODE_S]){
-		//TODO: ACA DEBERIA REINICIAR
+		State* stAux = RunGame::getInstance();
+		if(stAux == this->statePrevious)
+			st = IniGame::getInstance();
 	}
 	else if(keys[SDL_SCANCODE_P]){
 		st = this->statePrevious;
