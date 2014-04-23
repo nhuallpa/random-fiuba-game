84086fa70928e5c403242c487b06cfab182ab404
arielik@gmail.com <arielik@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-04-23 22:46:07 +0000

    Conexion del cliente

--- randomgame/src/utils/Network/Cliente.h
+++ randomgame/src/utils/Network/Cliente.h
@@ -1,54 +1,53 @@
-///*
-// * Cliente.h
-// *
-// *  Created on: 07/11/2013
-// *  Last Amended: 11/11/2013
-// *      Author: natuchis
-// */
-//
-//#ifndef CLIENTE_H_
-//#define CLIENTE_H_
-//
-//#include "Modelo/TipoJugador.h"
-//#include "Red/Socket.h"
-//#include "Red/TipoMensaje.h"
-//#include "Serializacion/Interno/Objeto.h"
-//#include "Threads/Atomic.h"
-//#include "Threads/Queue.h"
-//#include "Threads/Thread.h"
-//#include <string>
-//#include <vector>
-//
-//class Cliente {
-//public:
-//	Cliente();
-//	Cliente(int nroPuerto, char* hostName);
-//	~Cliente();
-//
-//	bool vivo ();
-//
-//	void push (Serializacion::Objeto delta);
-//	Serializacion::Objeto getCanonico ();
-//
-//	Modelo::TipoJugador getTipoJugador();
-//
-//private:
-//	Red::Socket fdEnt;
-//	Red::Socket fdSal;
-//	Modelo::TipoJugador idJugador;
-//
-//	// Se usa para controlar el timeout de los sockets
-//	time_t timeInicial;
-//
-//	Threads::Thread threadEsperarMensajes;
-//	Threads::Thread threadVerificarVida;
-//
-//	Threads::Atomic<Serializacion::Objeto> canonico;
-//	Threads::Queue<Serializacion::Objeto> deltasRed;
-//	Threads::Queue<Serializacion::Objeto> deltasLocal;
-//
-//	void esperarMensaje();
-//	void enviarMensaje(Red::TipoMensaje tipoMensaje, std::vector<uint8_t> buffer);
-//};
-//
-//#endif /* CLIENTE_H_ */
+#ifndef CLIENTE_H_
+#define CLIENTE_H_
+
+#include "Socket.h"
+#include "Messages.h"
+#include "Thread.h"
+#include <string>
+#include <vector>
+#include <list>
+
+typedef struct {
+	int wormid;
+	int action;
+} Playable;
+
+typedef struct {
+	std::string playerId;
+} Player;
+
+class Cliente {
+public:
+	Cliente();
+	Cliente(char* ip, int port);
+	~Cliente();
+
+	bool alive();
+
+
+private:
+	Socket input;
+	Socket output;
+
+	Player pl;
+
+	// Se usa para controlar el timeout de los sockets
+	time_t timeInicial;
+
+
+	std::list<Playable> localChanges;
+	std::list<Playable> networkChanges;
+
+	void esperarMensaje();
+	void sendMsg(Messages type, std::vector<uint8_t> buffer);
+};
+
+typedef struct{
+	Cliente* cli;
+	Socket clientO;
+	Socket clientI;
+	Player p;
+} threadData;
+
+#endif /* CLIENTE_H_ */
