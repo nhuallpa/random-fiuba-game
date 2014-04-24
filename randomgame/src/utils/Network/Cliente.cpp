c9be030ef45c06e9a6090da2a6673e0b64a41e0d
arielik@gmail.com <arielik@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-04-24 21:10:40 +0000

    Logica de desconexion de cliente finalizada

--- randomgame/src/utils/Network/Cliente.cpp
+++ randomgame/src/utils/Network/Cliente.cpp
@@ -21,34 +21,57 @@ Cliente::Cliente(char* ip, int port)
 	output.setKeepAlive();
 	output.setSendTimeout(5, 0);
 
-	input.connect2(ip, port);
-	printf("first connected");
-	output.connect2(ip, port + 1);
-	printf("second connected");
+	input.connect2(ip, port+1);
+	printf("Connected to data port: %d", port);
+	output.connect2(ip, port);
+	printf("Connected to update port: %d", port+1);
+	this->srvStatus = true;
 
-	//esperarMensaje(); // recibo el mundo o un codigo de reject
+	threadData data;
+	data.cli = this;
+
+	getRemoteWorld(); // recibo el mundo o un codigo de reject
 
 	//Thread de escucha de mensajes en la red
-
+	//Thread 
 
 	//Thread de escucha de actualizaciones locales
-
+	//Thread
 
 	//Thread de keepalive (Send dummies over network)
+	Thread keepaliveThread("KeepAlive",keepalive,&data);
 
 }
 
 Cliente::~Cliente() {
 }
 
-void Cliente::esperarMensaje() {
+int Cliente::keepalive(void* data){
+	printf("Disparado cliente");
+	Cliente* cli = ((threadData*)data)->cli;
+	Mutex* m = &((Cliente*)((threadData*)data)->cli)->m;
+	std::string* playerId = &((threadData*)data)->p;
+
+	std::vector<uint8_t> keepaliveData(10);
+	Messages keepaliveMsg = KEEPALIVE;
+
+	while(true){
+		cli->sendMsg(keepaliveMsg,keepaliveData);
+
+	}
+
+	return 0;
+}
+
+void Cliente::getRemoteWorld() {
 	Messages type;
 	std::vector<uint8_t> datos;
 	
 	if (!this->input.rcvmsg(type, datos)) {
 		//Log::e("connection error");
-		printf("Client: connection error");
+		printf("\nClient: connection error - Server disconnected/not responding");
 	}
+	printf("\nRetrieving data from server");
 
 	switch (type) {
 	case Messages::PLAYER:
@@ -69,8 +92,8 @@ void Cliente::esperarMensaje() {
 	}
 }
 
-bool Cliente::alive () {
-	return true;
+bool Cliente::serverAlive () {
+	return this->srvStatus;
 }
 
 //void Cliente::push (Playable localChange) {
