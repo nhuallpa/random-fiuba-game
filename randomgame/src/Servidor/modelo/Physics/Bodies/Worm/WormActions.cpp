b3f4226bcc866cadd81bcd45e1217d083ef3eb29
Bautista.Tusca@gmail.com <Bautista.Tusca@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-04-27 20:04:52 +0000

    Ahora, el worm sólo salta si su velocidad en el eje 'y' es igual a 0

--- randomgame/src/Servidor/modelo/Physics/Bodies/Worm/WormActions.cpp
+++ randomgame/src/Servidor/modelo/Physics/Bodies/Worm/WormActions.cpp
@@ -4,21 +4,25 @@
 WormActions::WormActions(Worm2d* worm2d)
 {
 	this->aWorm2d=worm2d;
-	
+
 }
 
 void WormActions::jump()
 {	
 	//si está en el suelo = cuando la velocidad en el eje y es igual a 0
-	float impulse = this->aWorm2d->getMass() * 10;
-    this->aWorm2d->body->ApplyLinearImpulse( b2Vec2(0,impulse), this->aWorm2d->body->GetWorldCenter() );
+	if(this->aWorm2d->body->GetLinearVelocity().y !=0)
+	{
+		float impulse = this->aWorm2d->getMass() * 10;
+		this->aWorm2d->body->ApplyLinearImpulse( b2Vec2(0,impulse), this->aWorm2d->body->GetWorldCenter() );
+	}
+
 }
 
 void WormActions::moveLeft()
 {
 	//aplicar fuerza hacia la izquierda
 	float impulse = this->aWorm2d->getMass() * 10;
-    this->aWorm2d->body->ApplyLinearImpulse( b2Vec2(-impulse,0), this->aWorm2d->body->GetWorldCenter() );
+	this->aWorm2d->body->ApplyLinearImpulse( b2Vec2(-impulse,0), this->aWorm2d->body->GetWorldCenter() );
 
 }
 
@@ -26,7 +30,7 @@ void WormActions::moveRight()
 {
 	//aplicar fuerza hacia la derecha
 	float impulse = this->aWorm2d->getMass() * 10;
-    this->aWorm2d->body->ApplyLinearImpulse( b2Vec2(impulse,0), this->aWorm2d->body->GetWorldCenter() );
+	this->aWorm2d->body->ApplyLinearImpulse( b2Vec2(impulse,0), this->aWorm2d->body->GetWorldCenter() );
 
 }
 
