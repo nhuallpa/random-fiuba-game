e2362ff3b662dd2295bc5e522a3a9e36159f2087
erikschmoll@gmail.com <erikschmoll@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-05-01 13:47:54 +0000

    eventos tp2: v.3.3, Refactor,
    Descripción: beta,

--- trunk/randomgame/src/Cliente/controlador/GameController.cpp
+++ trunk/randomgame/src/Cliente/controlador/GameController.cpp
@@ -9,37 +9,14 @@ GameController::GameController(){
 GameController::~GameController(){}
 void GameController::request(Contract* c){
 	detect();
-	//rg->setBind(&items);
 	rg->handle(c);
-	//call();
 	rg->call();
 }
 
 void GameController::handleState(Contract* c){
 	this->request(c);
 }
-/*
-void GameController::call(){
-	map<LISTENER,Event>::iterator it;
-	
-	it = items.find(SL);
-	if(it != items.end()){
-		this->notifierS((*it).second);
-		items.erase(it);
-	}
-	it = items.find(ZL);
-	if(it != items.end()){
-		this->notifierZ((*it).second);
-		items.erase(it);
-	}
-	it = items.find(CL);
-	if(it != items.end()){
-		this->notifierC((*it).second);
-		items.erase(it);
-	}
 
-}
-*/
 void GameController::detect(){
 	SDL_Event* ev = new SDL_Event();
 	SDL_StartTextInput();
@@ -64,7 +41,6 @@ bool GameController::isQuit(){
 	return quit;
 }
 
-
 bool GameController::isValidKey(SDL_Event* e){
 	return ((e->type == SDL_KEYDOWN) 
 		||  (e->type == SDL_KEYUP)
@@ -72,23 +48,19 @@ bool GameController::isValidKey(SDL_Event* e){
 		||  (e->type == SDL_MOUSEBUTTONDOWN)	);
 }
 
-
-
 void GameController::addListener(OnClickListener* c){
-	//lClistener.push_back(c);
 	Click* ck = Click::getInstance();
 	ck->add(c);
 }
 void GameController::addListener(OnMovementListener* m){
-	//lMlistener.push_back(m);
+	Move* mv = Move::getInstance();
+	mv->add(m);
 }
 void GameController::addListener(OnScrollListener* s){
 	Over* o = Over::getInstance();
-	//lSlistener.push_back(s);
 	o->add(s);
 }
 void GameController::addListener(OnZoomListener* z){
-	//lZlistener.push_back(z);
 	Zoom* zm = Zoom::getInstance();
 	zm->add(z);
 }
@@ -97,19 +69,8 @@ void GameController::remuveListener(OnClickListener* c){
 	ck->remuve(c);
 }
 void GameController::remuveListener(OnMovementListener* m){
-	/*list<OnMovementListener*>::iterator it;
-	list<OnMovementListener*>::iterator itDrop;
-	it = lMlistener.begin();
-	itDrop = lMlistener.end();
-	for(; it != lMlistener.end(); it++){
-		if((*it) == m){
-			itDrop = it;
-			break;
-		}
-	}
-	if(itDrop != lMlistener.end()){
-		lMlistener.erase(itDrop);
-	}*/
+	Move* mv = Move::getInstance();
+	mv->remuve(m);
 }
 void GameController::remuveListener(OnScrollListener* s){
 	Over* o = Over::getInstance();
@@ -119,104 +80,3 @@ void GameController::remuveListener(OnZoomListener* z){
 	Zoom* zm = Zoom::getInstance();
 	zm->remuve(z);
 }
-
-/*
-void GameController::notifier(pair<LISTENER, Event> item){
-	switch(item.first){
-	case CL: this->notifierC(item.second);
-		break;
-	case ML: this->notifierM(item.second);
-		break;
-	case SL: this->notifierS(item.second);
-		break;
-	case ZL: this->notifierZ(item.second);
-		break;
-	default: ;
-	};
-}
-
-
-void GameController::notifierC(Event e){
-	list<OnClickListener*>::iterator it;
-	ClickEvent c;
-	c.x = e.x, c.y = e.y;//, c.click = (Click) e.value;
-	it = lClistener.begin();
-	for(; it != lClistener.end(); it++){
-		(*it)->OnClick(c);
-	}
-}
-void GameController::notifierM(Event e){
-	list<OnMovementListener*>::iterator it;
-	MovementEvent m;
-	m.move = (Move)e.value;
-	it = lMlistener.begin();
-	for(; it != lMlistener.end(); it++){
-		(*it)->OnMovement(m);
-	}
-}
-void GameController::notifierS(Event e){
-	list<OnScrollListener*>::iterator it;
-	ScrollEvent s;
-	s.xSpeed = e.x, s.ySpeed = e.y;
-	it = lSlistener.begin();
-	for(; it != lSlistener.end(); it++){
-		(*it)->OnScroll(s);
-	}
-}*//*
-void GameController::notifierZ(Event e){
-	list<OnZoomListener*>::iterator it;
-	ZoomEvent z;
-	//z.zoom = (Zoom)e.value;
-	it = lZlistener.begin();
-	for(; it != lZlistener.end(); it++){
-		(*it)->OnZoom(z);
-	}
-}*/
-
-
-
-
-
-
-/*#include "GameController.h"
-
-GameController::GameController() {
-
-}
-
-GameController::~GameController() {
-}
-
-void GameController::handleState(Contract* c){
-	ctx.request(c);
-}
-
-bool GameController::isQuit(){
-	return ctx.isQuit();
-}
-
-
-void GameController::addListener(OnClickListener* c){
-	ctx.addListener(c);
-}
-void GameController::addListener(OnMovementListener* m){
-	ctx.addListener(m);
-}
-void GameController::addListener(OnScrollListener* s){
-	ctx.addListener(s);
-}
-void GameController::addListener(OnZoomListener* z){
-	ctx.addListener(z);
-}
-void GameController::remuveListener(OnClickListener* c){
-	ctx.addListener(c);
-}
-void GameController::remuveListener(OnMovementListener* m){
-	ctx.addListener(m);
-}
-void GameController::remuveListener(OnScrollListener* s){
-	ctx.addListener(s);
-}
-void GameController::remuveListener(OnZoomListener* z){
-	ctx.addListener(z);
-}*/
--- trunk/randomgame/src/Cliente/controlador/GameController.h
+++ trunk/randomgame/src/Cliente/controlador/GameController.h
@@ -3,20 +3,12 @@
 #ifndef GAMECONTROLLER_H_
 #define GAMECONTROLLER_H_
 
+#include <SDL.h>
 #include "RunGame.h"
 #include "Contracts\OnClickListener.h"
 #include "Contracts\OnMovementListener.h"
 #include "Contracts\OnScrollListener.h"
 #include "Contracts\OnZoomListener.h"
-#include <SDL.h>
-#include <list>
-#include <map>
-#include "Entity\Event.h"
-
-
-using namespace std;
-
-
 
 class GameController{
 	public:
@@ -34,58 +26,12 @@ class GameController{
 		void remuveListener(OnScrollListener* s);
 		void remuveListener(OnZoomListener* z);
 	private:
-		//list<OnClickListener*> lClistener;
-		//list<OnMovementListener*> lMlistener;
-		//list<OnScrollListener*> lSlistener;
-		//list<OnZoomListener*> lZlistener;
 		const Uint8* m_keys;
 		bool quit;
 		RunGame* rg;
 		void handleEvents(SDL_Event* e);
 		void detect();
 		bool isValidKey(SDL_Event* e);
-		//map<LISTENER, Event> items;
-		
-
-		//void call();
-		//void notifier(pair<LISTENER, Event> item);
-		//void notifierC(Event e);
-		//void notifierM(Event e);
-		//void notifierS(Event e);
-		//void notifierZ(Event e);
-
 };
 
 #endif 
-
-
-/*
-#pragma once
-
-#ifndef GAMECONTROLLER_H_
-#define GAMECONTROLLER_H_
-
-#include "Contracts\Contract.h"
-#include "Context.h"
-
-class GameController {
-public:
-	void handleState(Contract* c);
-	bool isQuit();
-	GameController();
-	~GameController();
-	void addListener(OnClickListener* c);
-	void addListener(OnMovementListener* m);
-	void addListener(OnScrollListener* s);
-	void addListener(OnZoomListener* z);
-	void remuveListener(OnClickListener* c);
-	void remuveListener(OnMovementListener* m);
-	void remuveListener(OnScrollListener* s);
-	void remuveListener(OnZoomListener* z);
-private:
-	Context ctx;
-
-};
-
-#endif // GAMECONTROLLER_H_ 
-*/
