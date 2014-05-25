340ecf65793b72875769e6303bb77255d799421d
huallpa.nestor@gmail.com <huallpa.nestor@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-05-25 17:15:40 +0000

    Agrego metodo para crear Texture Stream

--- src/Cliente/vista/EartView.h
+++ src/Cliente/vista/EartView.h
@@ -3,15 +3,29 @@
 #define __EART_VIEW__
 #include "View.h"
 #include "TextureManager.h"
+#include "FontManager.h"
 class EartView :
 	public View
 {
 	std::string imageId;
 
+	int counter;
+
+	SDL_Texture* mTexture;
+
+	/*this is filled in with a pointer to the locked pixels, appropriately offset by the locked area*/
+	void* mPixels;
+
+	/*Amount of bytes betweed rows */
+	int mPitch;
 public:
 	EartView(std::string imageId);
 	~EartView(void);
 	void draw(SDLScreen & screen);
+
+	void drawExplotion(int x, int y, int radius);
+
+	void convertToStreamTexture();
 
 };
 
