f88a6ef88ca766eda16e9b80944d7ee5dc255e70
erikschmoll@gmail.com <erikschmoll@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-06-05 00:52:57 +0000

    MenuWeaponView: 55%, versión: v.1.2

--- randomgame/src/Cliente/vista/MenuWeaponView.h
+++ randomgame/src/Cliente/vista/MenuWeaponView.h
@@ -2,13 +2,19 @@
 #define __MENU_WEAPON_VIEW__
 
 #include "View.h"
+#include "Weapon.h"
+
+using namespace std;
 
 class MenuWeaponView : public View{
 
 public:
 	MenuWeaponView(int x, int y);
 	~MenuWeaponView();
+	void buildWeapon();
 	void draw(SDLScreen & screen);
+private:
+	map<WeaponId, Weapon*> mapa;
 };
 
 
---
+++ randomgame/src/Cliente/vista/Weapon.cpp
@@ -0,0 +1,55 @@
+#include "Weapon.h"
+#include "TextureManager.h"
+
+
+Weapon::Weapon(WeaponId id, int x, int y, int width, int height)
+:View(x,y){
+	this->id = id, this->x = x, this->y = y;
+	this->width = width, this->height = height;
+	this->render = true;
+}
+
+Weapon::~Weapon(){
+
+}
+
+
+void Weapon::setRender(bool b){
+	this->render = b;
+}
+
+void Weapon::setState(WSTATE ws){
+	this->state = ws;
+}
+
+void Weapon::setWeapon(WSTATE ws, string ref){
+	this->weapons.insert(pair<WSTATE, string>(ws, ref));
+}
+
+string Weapon::getWeapon(WSTATE ws){
+	map<WSTATE, string>::iterator it;
+	it = this->weapons.find(ws);
+	if(it != this->weapons.end()){
+		return it->second;
+	}
+	return "";
+}
+
+
+void Weapon::draw(SDLScreen & screen){
+
+	if(!this->render)
+		return;
+
+	TextureManager::Instance().drawFrameOnScreen(
+					this->weapons[this->state], 
+					this->x, 
+					this->y, 
+					this->width, 
+					this->height, 
+					0, 
+					0, 
+					screen.getRenderer(),
+					false, 
+					SDL_FLIP_NONE);
+}
