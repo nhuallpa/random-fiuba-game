077055ba161f2e2b24a6615a544399529ec87793
arielik@gmail.com <arielik@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-06-11 21:02:29 +0000

    Modifico el radio de dibujo del circulo

--- randomgame/src/utils/Util.h
+++ randomgame/src/utils/Util.h
@@ -17,9 +17,9 @@ enum GameState {STARTED, WAITING_PLAYERS, OVER, WIN};
 enum UserDataPhysics { NO_UD=9, UD_TERRAIN=2, UD_WATER=0, UD_WORMS, UD_MISSIL };
 
 /* Explosion radius */
-#define EXPLODE_RSMALL 2.5
+#define EXPLODE_RSMALL 2
 #define EXPLODE_RMEDIUM 5
-#define EXPLODE_RLARGE 7.5
+#define EXPLODE_RLARGE 7
 #define EXPLODE_RGIANT 10
 
 /* Damage of Weapons */
