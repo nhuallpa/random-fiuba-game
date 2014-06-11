7e144281decd3e227b03add6e39e205221bf7391
erikschmoll@gmail.com <erikschmoll@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-06-11 00:16:20 +0000

    Registro a la mira

--- randomgame/src/Cliente/vista/GameActivity.cpp
+++ randomgame/src/Cliente/vista/GameActivity.cpp
@@ -39,6 +39,7 @@ void GameActivity::buildView()
 	aimView = new AimView();
 	gameView->add(aimView);
 	this->cController->addOnCoordListener(aimView);
+	this->cController->addOnMovementListener(aimView);
 }
 
 void GameActivity::update() 
