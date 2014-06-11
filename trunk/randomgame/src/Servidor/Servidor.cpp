0430bf5eec1ccb93159093e59e44e82287c0ae15
arielik@gmail.com <arielik@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-06-11 17:06:01 +0000

    bug en elementId corregido

--- trunk/randomgame/src/Servidor/Servidor.cpp
+++ trunk/randomgame/src/Servidor/Servidor.cpp
@@ -213,12 +213,12 @@ int Servidor::somethingChange(){
 	for ( ; it != copy.end() ; ++it){
 		if( it->second->changed ){
 			
-				//this->worldQ.play[i].wormid = it->second->getId();
+				//
 				//printf("\nSending weapon: %d at position %f, %f",it->second->getWeaponId(),it->second->getPosition().first,
 				//	it->second->getPosition().second );
 				//printf("\nAction: %d",it->second->getAction() );
 
-
+			this->worldQ.play[i].wormid = it->second->getId();
 			this->worldQ.play[i].x = it->second->getPosition().first;
 			this->worldQ.play[i].y = it->second->getPosition().second;
 			this->worldQ.play[i].life = it->second->getLife();
