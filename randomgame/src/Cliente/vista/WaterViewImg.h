8e16233ad8bbbf1e98e71e692e0174334a2e3c4d
erikschmoll@gmail.com <erikschmoll@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-06-23 02:03:48 +0000

    Ajusto el agua

--- randomgame/src/Cliente/vista/WaterViewImg.h
+++ randomgame/src/Cliente/vista/WaterViewImg.h
@@ -12,19 +12,25 @@ class WaterViewImg : public View{
 
 public:
 	WaterViewImg(float timmerChange, bool bBackground);
+	WaterViewImg();
 	~WaterViewImg();
+	void initialize(int Gap, float timmerChange,bool bBackground);
 	void actionWater();
 	void draw(SDLScreen & screen);
 	static WaterViewImg* FactoryWater(int Gap, float timmerChange, bool bBackground);
+	static WaterViewImg* FactoryWater2(int Gap, float timmerChange, bool bBackground);
 private:
 	Timer timer;
 	int timmerChange;
+	int Gap;
 	bool bRender,
 		 bBackground;
 	map<string, Shape> water;
 	pair<string, Shape> backgroundWater; 
 	map<string, Shape>::iterator it;
 	void loadWater(int Gap);
+	void loadWater2(int Gap);
+	void loadWater3(int Gap);
 	void iniView();
 	void iniRender();
 	void controlRender();
