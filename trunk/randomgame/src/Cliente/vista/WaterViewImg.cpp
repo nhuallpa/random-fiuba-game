4de64844b6f17f81377fbe160f2347f43bc55947
erikschmoll@gmail.com <erikschmoll@gmail.com@658c8fdb-a348-8819-cda3-7219524bffda>
2014-06-23 02:03:48 +0000

    Ajusto el agua

--- trunk/randomgame/src/Cliente/vista/WaterViewImg.cpp
+++ trunk/randomgame/src/Cliente/vista/WaterViewImg.cpp
@@ -1,6 +1,8 @@
 #include "WaterViewImg.h"
 #include "TextureManager.h"
 
+WaterViewImg::WaterViewImg():View(1,1){
+}
 
 WaterViewImg* WaterViewImg::FactoryWater(int Gap, float timmerChange,bool bBackground){
 	WaterViewImg* wvi = new WaterViewImg(timmerChange, bBackground);
@@ -11,9 +13,28 @@ WaterViewImg* WaterViewImg::FactoryWater(int Gap, float timmerChange,bool bBackg
 }
 
 
+void WaterViewImg::initialize(int Gap, float timmerChange,bool bBackground){
+	this->timmerChange = timmerChange;
+	this->bBackground = bBackground;
+	this->Gap = 35;
+	this->enableScroll();
+	this->loadWater(Gap);
+	this->iniView();
+	this->iniRender();
+}
+
+WaterViewImg* WaterViewImg::FactoryWater2(int Gap, float timmerChange,bool bBackground){
+	WaterViewImg* wvi = new WaterViewImg(timmerChange, bBackground);
+	wvi->loadWater2(Gap);
+	wvi->iniView();
+	wvi->iniRender();
+	return wvi;
+}
+
 WaterViewImg::WaterViewImg(float timmerChange, bool bBackground):View(0,30){
 	this->timmerChange = timmerChange;
 	this->bBackground = bBackground;
+	this->Gap = 35;
 	this->enableScroll();
 }
 
@@ -44,7 +65,7 @@ void WaterViewImg::draw(SDLScreen & screen){
 	if(bBackground){
 		this->drawbackground(screen);
 	}
-	//this->drawSurf(screen);
+	this->drawSurf(screen);
 }
 
 void WaterViewImg::drawSurf(SDLScreen & screen){
@@ -64,7 +85,7 @@ void WaterViewImg::drawSurf(SDLScreen & screen){
 	TextureManager::Instance().drawFrame(
 								it->first, 
 								this->getX(),//point.first, 
-								this->getY() - 80,//point.second,
+								this->getY() - this->Gap,//point.second,
 								dimen.first, 
 								dimen.second, 
 								0, 0, 
@@ -111,13 +132,83 @@ void WaterViewImg::loadWater(int Gap){
 	height = lebel;
 
 	water.insert(pair<string,Shape>("marea_1_1", 
-		Shape(positionX, positionY - Gap, width, height + Gap)));
+		Shape(positionX, positionY - Gap, width, Gap)));
 	water.insert(pair<string,Shape>("marea_1_2", 
-		Shape(positionX, positionY - Gap, width, height + Gap)));
+		Shape(positionX, positionY - Gap, width, Gap)));
 	water.insert(pair<string,Shape>("marea_1_3", 
-		Shape(positionX, positionY - Gap, width, height + Gap)));
+		Shape(positionX, positionY - Gap, width, Gap)));
 	water.insert(pair<string,Shape>("marea_1_4", 
-		Shape(positionX, positionY - Gap, width, height + Gap)));
+		Shape(positionX, positionY - Gap, width, Gap)));
+
+	this->setY(positionY);
+	backgroundWater = pair<string,Shape>("FondoAgua", 
+		Shape(positionX, positionY, width, height));
+
+}
+
+void WaterViewImg::loadWater2(int Gap){
+	int positionX, positionX1,
+		positionY, positionX2,
+		lebel, 
+		width, width2,
+		height, height2;
+	pair<int, int> dimension;
+	
+	dimension = TextureManager::Instance().getDimension("eart");
+	lebel = (int)(Util::string2float(ParserYaml::
+		getInstance()->getEscenarioAgua()) 
+							* ESCALA_UL2PX);
+	
+	positionX = 0;
+	this->Gap += Gap;
+	positionY = dimension.second - lebel + 8 + 60;
+	width = dimension.first;
+	height = lebel;
+
+	water.insert(pair<string,Shape>("marea_1_1", 
+		Shape(positionX, positionY - Gap, width, Gap)));
+	water.insert(pair<string,Shape>("marea_1_2", 
+		Shape(positionX, positionY - Gap, width, Gap)));
+	water.insert(pair<string,Shape>("marea_1_3", 
+		Shape(positionX, positionY - Gap, width, Gap)));
+	water.insert(pair<string,Shape>("marea_1_4", 
+		Shape(positionX, positionY - Gap, width, Gap)));
+
+	this->setY(positionY);
+	backgroundWater = pair<string,Shape>("FondoAgua", 
+		Shape(positionX, positionY, width, height));
+
+}
+
+
+
+void WaterViewImg::loadWater3(int Gap){
+	int positionX, positionX1,
+		positionY, positionX2,
+		lebel, 
+		width, width2,
+		height, height2;
+	pair<int, int> dimension;
+	
+	dimension = TextureManager::Instance().getDimension("eart");
+	lebel = (int)(Util::string2float(ParserYaml::
+		getInstance()->getEscenarioAgua()) 
+							* ESCALA_UL2PX);
+	
+	positionX = 0;
+	this->Gap += Gap;
+	positionY = dimension.second - lebel - 40;
+	width = dimension.first;
+	height = lebel;
+
+	water.insert(pair<string,Shape>("marea_1_1", 
+		Shape(positionX, positionY - Gap, width, Gap)));
+	water.insert(pair<string,Shape>("marea_1_2", 
+		Shape(positionX, positionY - Gap, width, Gap)));
+	water.insert(pair<string,Shape>("marea_1_3", 
+		Shape(positionX, positionY - Gap, width, Gap)));
+	water.insert(pair<string,Shape>("marea_1_4", 
+		Shape(positionX, positionY - Gap, width, Gap)));
 
 	this->setY(positionY);
 	backgroundWater = pair<string,Shape>("FondoAgua", 
